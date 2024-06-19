4f064494f7b5346baa7d6eacc314bb7801105fd1
ahsan <ahsanahmadsn@gmail.com>
2024-06-19 09:31:16 +0500

    appointment service

--- src/app/shared/appointment.service.ts
+++ src/app/shared/appointment.service.ts
@@ -1,32 +1,44 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { Appointment } from './appointment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AppointmentService {
-  private appointmentsSubject = new BehaviorSubject<Appointment[]>([]);
-  appointments$ = this.appointmentsSubject.asObservable();
+  private appointmentsSubject: BehaviorSubject<Appointment[]> = new BehaviorSubject<Appointment[]>([]);
+  public appointments$: Observable<Appointment[]> = this.appointmentsSubject.asObservable();
 
-  addAppointment(appointment: Appointment) {
+  constructor() {
+
+    const initialAppointments: Appointment[] = [];
+    this.appointmentsSubject.next(initialAppointments);
+  }
+
+  private getNextId(): number {
+    const currentAppointments = this.appointmentsSubject.value;
+    return currentAppointments.length > 0 ? Math.max(...currentAppointments.map(a => a.id)) + 1 : 1;
+  }
+
+  addAppointment(appointment: Appointment): void {
+    appointment.id = this.getNextId();
     const currentAppointments = this.appointmentsSubject.value;
     this.appointmentsSubject.next([...currentAppointments, appointment]);
   }
 
-  deleteAppointment(id: string) {
-    const currentAppointments = this.appointmentsSubject.value.filter(appointment => appointment.id !== id);
-    this.appointmentsSubject.next(currentAppointments);
-  }
-
-  updateAppointment(updatedAppointment: Appointment) {
+  updateAppointment(updatedAppointment: Appointment): void {
     const currentAppointments = this.appointmentsSubject.value.map(appointment =>
       appointment.id === updatedAppointment.id ? updatedAppointment : appointment
     );
     this.appointmentsSubject.next(currentAppointments);
   }
 
-  updateAppointments(appointments: Appointment[]) {
+  deleteAppointment(id: number): void {
+    const currentAppointments = this.appointmentsSubject.value.filter(appointment => appointment.id !== id);
+    this.appointmentsSubject.next(currentAppointments);
+  }
+
+  updateAppointments(appointments: Appointment[]): void {
     this.appointmentsSubject.next(appointments);
   }
 }
