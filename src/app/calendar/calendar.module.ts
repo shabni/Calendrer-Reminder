c137861f7999b9784772590f72a8872430d65f1f
ahsan <ahsanahmadsn@gmail.com>
2024-06-19 09:04:07 +0500

    Add calendar module

---
+++ src/app/calendar/calendar.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CalendarRoutingModule } from './calendar-routing.module';
+import { CalendarComponent } from './calendar.component';
+import { CalendarViewComponent } from './calendar-view/calendar-view.component';
+import { AppointmentFormComponent } from './appointment-form/appointment-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatIconModule } from '@angular/material/icon';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+
+
+@NgModule({
+  declarations: [
+    CalendarComponent,
+    CalendarViewComponent,
+    AppointmentFormComponent
+  ],
+  imports: [
+    CommonModule,
+    CalendarRoutingModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatIconModule,
+    DragDropModule
+  ]
+})
+export class CalendarModule { }
