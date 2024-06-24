b58865093d04baa1a232c0a36eea3fbea18b37d8
ahsan <ahsanahmadsn@gmail.com>
2024-06-24 15:04:35 +0500

    add wild card

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [{ path: 'calendar', loadChildren: () => import('./calendar/calendar.module').then(m => m.CalendarModule) },
-  { path: '', redirectTo: '/calendar', pathMatch: 'full' }
+  { path: '**', redirectTo: '/calendar', pathMatch: 'full' }
 ];
 
 @NgModule({
